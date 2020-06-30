f00e0fd82a737575f14322ce289ec253476958ac
brandon-elzy <brandon.g.elzy@line-zero.com>
2020-06-30 15:14:16 -0500

    Added function to receive json data

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,23 @@
 (function() {
 
-    
+    const getData = done => {
+
+        const resource = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
+        const init = {
+            method: 'GET',
+        };
+
+        fetch(resource, init)
+            .then(response => response.json())
+            .then(data => {
+                done(data);
+            })
+            .catch(err => console.log(err));
+
+    };
+
+    getData(data => {
+        console.log(data);
+    });
 
 })();
